4773f8a8f71ee184015e9cab2c745b53c2045764
Eric Woolsey <ewoolsey@ualberta.ca>
2024-11-21 11:26:23 -0800

    specify rust version

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -11,7 +11,7 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
         "gopls",
-        "rust_analyzer",
+        "rust_analyzer@2024-10-21",
       })
     end,
   },
